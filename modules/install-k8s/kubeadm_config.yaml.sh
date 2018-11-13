3ef6e656f32021e363fbf90e7715a5579612cda4
Yann DEGAT <yann.degat@gmail.com>
2018-11-13 14:47:56 +0100

    Fix coredns configmap for kubeadm

--- modules/install-k8s/kubeadm_config.yaml.sh
+++ modules/install-k8s/kubeadm_config.yaml.sh
@@ -29,4 +29,29 @@ nodeName: $(hostname)
 authorizationModes:
 $(echo "${AUTHORIZATION_MODES:-Node,RBAC}" | cut -d, -f1- --output-delimiter=$'\n' | sed 's/\(.*\)/- \1/g')
 selfHosted: false
+---
+kind: ConfigMap
+apiVersion: v1
+metadata:
+  name: coredns
+  namespace: kube-system
+  resourceVersion: "214"
+  selfLink: /api/v1/namespaces/kube-system/configmaps/coredns
+data:
+  Corefile: |
+    .:53 {
+        errors
+        health
+        kubernetes ${CLUSTER_DOMAIN} in-addr.arpa ip6.arpa {
+           pods insecure
+           upstream
+           fallthrough in-addr.arpa ip6.arpa
+        }
+        prometheus :9153
+        proxy . ${UPSTREAM_RESOLVER}
+        cache 30
+        loop
+        reload
+        loadbalance
+    }
 EOF
