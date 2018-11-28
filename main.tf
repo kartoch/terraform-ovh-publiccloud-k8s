a10345952d33224d91552a6d43483135156aa9fe
Yann DEGAT <yann.degat@gmail.com>
2018-11-28 10:37:04 +0100

    etcd: bump to v0.1.8

--- main.tf
+++ main.tf
@@ -186,7 +186,7 @@ module "post_install_cfssl" {
 
 module "post_install_etcd" {
   source  = "ovh/publiccloud-etcd/ovh//modules/install-etcd"
-  version = "0.1.7"
+  version = "0.1.8"
 
   count            = "${var.post_install_modules && var.etcd ? var.count : 0}"
   triggers         = ["${concat(openstack_compute_instance_v2.singlenet_k8s.*.id, openstack_compute_instance_v2.multinet_k8s.*.id)}"]
--- modules/k8s-userdata/main.tf
+++ modules/k8s-userdata/main.tf
@@ -26,7 +26,7 @@ module "cfssl" {
 
 module "etcd" {
   source               = "ovh/publiccloud-etcd/ovh//modules/etcd-userdata"
-  version              = "0.1.7"
+  version              = "0.1.8"
   count                = "${var.count}"
   name                 = "${var.name}"
   domain               = "${var.domain}"
